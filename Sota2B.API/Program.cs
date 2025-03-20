70b114b43cdcf963a1f90ea6af14f28f29fe43b3
Глеб Попов <0504051@mail.ru>
2025-03-21 00:51:17 +0300

    automigration

--- Sota2B.API/Program.cs
+++ Sota2B.API/Program.cs
@@ -37,4 +37,14 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<Sota2BContext>();
+    if (context.Database.GetPendingMigrations().Any())
+    {
+        context.Database.Migrate();
+    }
+}
 app.Run();
